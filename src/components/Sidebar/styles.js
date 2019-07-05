ba566ee740ce85ffd6fa2e1e431fab9897233872
Max Arouca <maximiler@gmail.com>
2019-07-05 18:37:13 -0300

    Ajustes: Rotas Privadas, Sidebar Lateral e Menu Superior

---
+++ src/components/Sidebar/styles.js
@@ -0,0 +1,53 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const styles = makeStyles(theme => ({
+  root: {},
+  aside: {
+    width: 300,
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: theme.palette.primary.dark,
+    height: '100vh',
+  },
+  sectionLogo: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 64,
+    width: '100%',
+    backgroundColor: theme.palette.primary.darker,
+    color: theme.palette.primary.lighter,
+  },
+  logoImg: {
+    width: 130,
+    height: 35,
+  },
+  user: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '15px 0',
+  },
+  nav: {
+    color: theme.palette.primary.light,
+  },
+  avatar: {
+    fontSize: 70,
+    color: theme.palette.primary.lighter,
+    marginRight: 15,
+  },
+  listItem: {
+    borderLeft: `8px solid ${theme.palette.primary.dark}`,
+  },
+  selected: {
+    backgroundColor: `${theme.palette.primary.darker} !important`,
+    borderLeft: `8px solid ${theme.palette.primary.main}`,
+  },
+  iconLink: {
+    color: theme.palette.primary.light,
+  },
+}))
+
+export default styles
