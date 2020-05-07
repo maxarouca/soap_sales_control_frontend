a584ce1f8b9eba33b18dd03ecfa88085647abca5
Maximiler Arouca <maximiler@gmail.com>
2020-05-06 23:56:09 -0300

    Correção HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://157.245.222.156:3333',
+  baseURL: 'https://cors-anywhere.herokuapp.com/http://157.245.222.156:3333',
 });
 
 export default api;
